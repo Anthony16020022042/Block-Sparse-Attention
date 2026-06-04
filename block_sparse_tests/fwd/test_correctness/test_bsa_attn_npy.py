8c54a195e9785f7e6783df55179ccbc280ba157b
wjc <wangjincheng14@huawei.com>
2026-06-04 15:35:50 +0800

    feat:tmp

---
+++ block_sparse_tests/fwd/test_correctness/test_bsa_attn_npy.py
@@ -0,0 +1,53 @@
+import pytest
+import torch
+import torch_npu
+from einops import repeat
+from block_sparse_attn import block_sparse_attn_func
+
+test_cases = [
+    # (data_type, batch_size, num_heads, kv_heads, q_seqlen, kv_seqlen, head_size, is_causal)
+    (torch.bfloat16, 1, 1, 1, 512, 1024, 128, True)
+]
+
+@pytest.mark.parametrize("data_type, batch_size, num_heads, kv_heads, q_seqlen, kv_seqlen, head_size, is_causal", test_cases)
+def test_bsa_varlen_ops(data_type, batch_size, num_heads, kv_heads, q_seqlen, kv_seqlen, head_size, is_causal):
+    q_min_range = -5.0
+    q_max_range = 5.0
+    kv_min_range = -5.0
+    kv_max_range = 5.0
+    query = (q_min_range + (q_max_range - q_min_range) * torch.rand(batch_size * q_seqlen, num_heads, head_size)).to(data_type).npu()
+    key = (kv_min_range + (kv_max_range - kv_min_range) * torch.rand(batch_size * kv_seqlen, kv_heads, head_size)).to(data_type).npu()
+    value = (kv_min_range + (kv_max_range - kv_min_range) * torch.rand(batch_size * kv_seqlen, kv_heads, head_size)).to(data_type).npu()
+    actual_seq_len = torch.tensor([q_seqlen * i for i in range(batch_size + 1)], dtype=torch.int32).npu()
+    actual_kv_len = torch.tensor([kv_seqlen * i for i in range(batch_size + 1)], dtype=torch.int32).npu()
+
+    max_seqlen_q = q_seqlen
+    max_seqlen_k = kv_seqlen
+    dropout_p = 0.0
+    scale = 1.0 / (head_size ** 0.5)
+    window_size_left = -1
+    window_size_right = -1
+    return_attn_probs = False
+    block_table = None
+    head_mask_type = None
+    streaming_info = None
+    base_blockmask = None
+
+    out_unpad, sm_lse, S_dmask = block_sparse_attn_func(
+        query, 
+        key, 
+        value,
+        actual_seq_len, 
+        actual_kv_len,
+        head_mask_type,
+        streaming_info,
+        base_blockmask,
+        max_seqlen_q, 
+        max_seqlen_k,
+        dropout_p,
+        deterministic=True,
+        softmax_scale=scale,
+        is_causal=is_causal,
+        exact_streaming=False,
+        return_attn_probs=return_attn_probs,
+    )
