265da8b70bc5bf29cdd1c64c45989787947007b3
wjc <wangjincheng14@huawei.com>
2026-06-11 20:19:29 +0800

    feat:tmp

--- block_sparse_tests/fwd/test_correctness/test_bsa_attn_npy.py
+++ block_sparse_tests/fwd/test_correctness/test_bsa_attn_npy.py
@@ -495,11 +495,11 @@ test_cases = [
     # (data_type, batch_size, num_heads, kv_heads, q_seqlen, kv_seqlen, head_size, is_causal)
     (torch.bfloat16, 1, 1, 1, 512, 1024, 128, True),
     (torch.bfloat16, 2, 4, 4, 1024, 1024, 128, False),
-    (torch.float16, 7, 5, 1, 512, 512, 128, True),
-    (torch.float16, 7, 5, 1, 777, 888, 192, False),
-    (torch.float16, 7, 5, 1, 1777, 1888, 256, True),
-    (torch.bfloat16, 1, 1, 1, 7777, 8192, 64, True),
-    (torch.bfloat16, 7, 5, 1, 711, 8192, 111, True)
+    # (torch.float16, 7, 5, 1, 512, 512, 128, True),
+    # (torch.float16, 7, 5, 1, 777, 888, 192, False),
+    # (torch.float16, 7, 5, 1, 1777, 1888, 256, True),
+    # (torch.bfloat16, 1, 1, 1, 7777, 8192, 64, True),
+    # (torch.bfloat16, 7, 5, 1, 711, 8192, 111, True)
 ]
 
 def print_tensor_full(name, tensor):
